2cf1de2c4a7a301d7751df2c0cbcc3328ee717a2
Fateh <fateh137@gmail.com>
2020-05-30 04:05:12 +0100

    change end point

--- src/apis/Api.js
+++ src/apis/Api.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 
 let BaseApi = axios.create({
-	baseURL: "https://3b43f4671829.ngrok.io/api",
+	baseURL: "https://b26cbe65c4ca.ngrok.io/api",
 });
 
 let Api = function() {
-	let token = localStorage.getItem("token");
+  let token = localStorage.getItem("token");
 
-	if (token) {
-		BaseApi.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-	}
+  if (token) {
+    BaseApi.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  }
 
-	return BaseApi;
+  return BaseApi;
 };
 
 export default Api;
